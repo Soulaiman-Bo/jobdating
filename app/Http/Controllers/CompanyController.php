ccca41abfef3e78de74fb1c72f4c96825d3f0955
Soulaiman-Bo <bouhlal.soulaiman@gmail.com>
2024-01-29 16:59:12 +0100

    i added create method into the CompanyController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use Exception;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('companies.index');
+    }
+
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    public function store(Request $req)
+    {
+        $data = $req->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'logo' => 'required',
+            'sector' => 'nullable',
+            'location' => 'nullable'
+        ]);
+
+        $company = Company::create($data);
+
+        return redirect()->route('company.index')->with("success", 'Inserted Successfully');
     }
 }
