0f1a982149f00418bfdff4870899896dca19908e
Soulaiman-Bo <bouhlal.soulaiman@gmail.com>
2024-01-31 11:16:49 +0100

    I added announcements migrations

--- database/migrations/2024_01_31_091701_create_announcements_table.php
+++ database/migrations/2024_01_31_091701_create_announcements_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255)->unique();
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->integer('company_id')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_01_31_093544_adding_foreign_key_to_announcmenets.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::table('announcements', function (Blueprint $table) {
+            $table->foreign('company_id')
+            ->references('id')
+            ->on('companies')
+            ->onDelete('cascade')
+            ->constrained();
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('announcmenets', function (Blueprint $table) {
+            //
+        });
+    }
+};
